117f51fa05eaf68fb5627e8c47397e6858be7d59
Alon Barak <alonbarak3110@gmail.com>
2022-03-07 01:55:51 +0200

    started function mat

--- mat.cpp
+++ mat.cpp
@@ -6,6 +6,21 @@ using namespace std;
 namespace ariel {
 
     string mat(int rows, int cols, char c1, char c2){
+
+        if((rows % 2 == 0) || (cols % 2 == 0)){
+            throw "Mat size is always odd";
+        }
+
+        if((rows < 0) || (cols < 0)){
+            throw "Mat size can't take negative numbers as input";
+        }
+
+        
+
+
+
+
+
         return "";
     }    
 
