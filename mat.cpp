1c5e10fc7495b41277b6ea0c41ee9e653aa4e3f1
Alon Barak <alonbarak3110@gmail.com>
2022-03-07 16:03:34 +0200

    update timeout

--- mat.cpp
+++ mat.cpp
@@ -7,13 +7,13 @@ namespace ariel {
 
     string mat(int rows, int cols, char c1, char c2){
 
-        if((rows % 2 == 0) || (cols % 2 == 0)){
-            throw "Mat size is always odd";
-        }
+        // if((rows % 2 == 0) || (cols % 2 == 0)){
+        //     throw "Mat size is always odd";
+        // }
 
-        if((rows < 0) || (cols < 0)){
-            throw "Mat size can't take negative numbers as input";
-        }
+        // if((rows < 0) || (cols < 0)){
+        //     throw "Mat size can't take negative numbers as input";
+        // }
 
         
 
