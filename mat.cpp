ce4335a140684ccb50359ae338dc6ab066bca208
Alon Barak <alonbarak3110@gmail.com>
2022-03-08 17:26:55 +0200

    issues

--- mat.cpp
+++ mat.cpp
@@ -1,56 +1,29 @@
+#include "mat.hpp"
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
 
 namespace ariel {
 
-    string mat(int rows, int cols, char c1, char c2){
-
-        if((rows % 2 == 0) || (cols % 2 == 0)){
-            throw "Mat size is always odd";
-        }
-
-        if((rows < 0) || (cols < 0)){
-            throw "Mat size can't take negative numbers as input";
-        }
-
-        string res_string = "";
-        int times = 0;
-        for (int i = 0; i < rows; i++)
-        {
-            times = (i + 1) / 2;
-
-            if(i % 2 == 0){
-                // even row index
-                res_string = res_string + even(c1, c2, times, cols);
-            }
-            else{
-                // odd row index
-                res_string = res_string + odd(c1, c2, times, cols);
-            }
-            if(i != rows - 1){
-                // step one line down
-                res_string += "\n";
-            }
-        }
-        return res_string;
-    }    
 
     string even(char c1, char c2, int times, int cols){
         /*
             this method return a custom string for 
             even rows.
         */
-        string res_string = "";
+        string res_string;
         for (int i = 0; i < cols; i++)
         {
-            
+            res_string.push_back(c1);
         }
-        
-
-
-        return "";
+        for(int i = 0; i < times; i++){
+            
+            res_string[2*i + 1] = c2;
+            res_string[cols - 2*i - 2] = c2;
+        }
+        return res_string;
     }
 
     string odd(char c1, char c2, int times, int cols){
@@ -59,7 +32,72 @@ namespace ariel {
             odd rows.
         */
 
-        return "";
+        string res_string;
+        for(int i = 0; i < cols; i++){
+            res_string.push_back(c2);
+        }
+        for(int i = 0; i < times; i++){
+            
+            res_string[2*i] = c1;
+            res_string[cols - 2*i - 1] = c1;
+        }
+
+        return res_string;
     }
 
+
+    string mat(int col, int row, char c1, char c2){
+
+        if ( (row % 2 == 0) || (col % 2 == 0)) {
+            throw runtime_error("Rows and Cols size is invalid, try odd values");
+        }
+        if ((row < 0) || (col < 0)) {
+            throw runtime_error("Rows and Cols size is invalid, try positive values");
+        }
+
+        string res_string;
+        string buttom_half;
+        string line;
+        int times = 0;
+        for (int i = 0; i < (row / 2 + 1); i++)
+        {
+            times = (i + 1) / 2; 
+
+            if(i % 2 == 0){
+                // even row index
+                line = even(c1, c2, times, col);
+                res_string.append(line);
+                if(i != int(row / 2)){
+                    buttom_half.insert(0, "\n");
+                    buttom_half.insert(0, line);
+                }
+                
+            }
+            else{
+                // odd row index
+                line = odd(c1, c2, times, col);
+                res_string.append(line);
+                if(i != int(row / 2)){
+                    buttom_half.insert(0, "\n");
+                    buttom_half.insert(0, line);
+                }
+            }
+            if(i != row - 1){
+                // step one line down
+                res_string.append("\n");
+            }
+        }
+        res_string.append(buttom_half);
+        return res_string;
+    }    
+
 }
+
+
+// int main(){
+
+
+//     cout << ariel::mat(1,1,'-',',') << endl;
+
+//     return 0;
+// }
--- mat.hpp
+++ mat.hpp
@@ -6,6 +6,8 @@ using namespace std;
 
 namespace ariel
 {
+    string even(char c1, char c2, int times, int cols);
+    string odd(char c1, char c2, int times, int cols);
     string mat(int rows, int cols, char c1, char c2);
 }
 
